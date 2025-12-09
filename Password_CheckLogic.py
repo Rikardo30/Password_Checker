2b257612a015b225c6cc49bc966b6aa0177ff1fb
Rikardo30 <ricardoherrera.3030@gmail.com>
2025-12-08 23:38:48 -0600

    Implement password checker logic

---
+++ Password_CheckLogic.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def display_welcome_message():
+    return (
+        "==============================================\n"
+        "|      Welcome to Password Void Checker      |\n"
+        "==============================================\n"
+        "                                              \n"
+        "    This tools purpose is to test, create,    \n"
+        "           and store your password            \n")
